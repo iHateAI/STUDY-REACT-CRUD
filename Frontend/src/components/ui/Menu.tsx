a52190b6faa24f9e88277ea97389d0e0f8230cf0
HyunSeong <bolton7425@gmail.com>
2023-08-01 00:01:22 +0900

    FEAT: Menu 컴포넌트 생성 및 스타일 적용

---
+++ Frontend/src/components/ui/Menu.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const StyledList = styled.ul`
+  list-style: none;
+  height: 100px;
+  margin: 0;
+`;
+
+const StyledItem = styled.li`
+  width: 100px;
+  display: inline-block;
+  height: 100px;
+  line-height: 100px;
+  cursor: pointer;
+`;
+
+function Menu(props: any): JSX.Element {
+  
+
+  return (
+    <StyledList>
+      <StyledItem>글 목록</StyledItem>
+      <StyledItem>글 생성</StyledItem>
+    </StyledList>
+  );
+}
+
+export default Menu;
